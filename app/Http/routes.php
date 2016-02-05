45c931d8e379815baf1c67e09b754970bf01d524
Justyn Tenandar <justyntenandar@internal.baisedu.org>
2016-02-05 09:30:24 +0700

    Added a default page for BoB

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/bob/', function() {
+    return view('bob-home');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
---
+++ resources/views/bob-home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+@section('title', 'Welcome!')
+@section('content')
+    <div class="content">
+        <div class="title"><h1>Battle of the Books</h1></div>
+        <nav class="navbar navbar-default">
+        <div class="container-fluid">
+        <div class="navbar-header">
+        <a class="navbar-brand" href="#">WebSiteName</a>
+    <5/5div>
+    <div>
+        <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li> 
+        <li><a href="#">Page 3</a></li> 
+        </ul>
+    </div>
+        </nav>
+        
+
+
+
+
+
+    </div>
+@endsection
