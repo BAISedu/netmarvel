1066ec2222adf2c5fcbbdf50bc8304cfeffbc2c3
Matthew Shin <matthewshin@internal.baisedu.org>
2016-02-05 09:28:26 +0700

    Added default home pages for cross-country

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/crosscountry /', function () {
+    return view('crosscountry-home');
+});
 /*
 |--------------------------------------------------------------------------
 | Application Routes
