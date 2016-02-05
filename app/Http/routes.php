f414c393b089fcc266c84714206fa9b11ed7751d
Brenden Lyon <brendenlyon@internal.baisedu.org>
2016-02-05 09:28:40 +0700

    Work I did on Feb. 5

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/basketball/', function() {
+    return view('basketball-home', ['year' => date('Y')]);
+}); 
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
