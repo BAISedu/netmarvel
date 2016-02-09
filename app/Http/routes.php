97eee525849ef94535ce6babdcea8fb506076fa1
Ji Ho Yoo <jihoyoo@internal.baisedu.org>
2016-02-09 09:00:27 +0700

    Added a basic home pages for badminton application

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/badminton/', function () {
+    return view('badminton-home');
+});
 /*
 |--------------------------------------------------------------------------
 | Application Routes
