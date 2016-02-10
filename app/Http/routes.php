9a0909106901b83a91fa226a6fed467b72e42cb4
Ji Ho Yoo <jihoyoo@internal.baisedu.org>
2016-02-10 08:43:12 +0700

    resolved conflicts

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,11 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::get('/basketball/', function() {
     return view('basketball-home', ['year' => date('Y')]);
 }); 
-=======
+
 Route::get('/soccer/', function() {
     return view('soccer-home');
 });
@@ -43,7 +42,7 @@ Route::get('/swim/event-detail/', function () {
 Route::get('/crosscountry /', function () {
     return view('crosscountry-home');
 });
->>>>>>> 8c2d67720bce895afa24a381182655db23dc8677
+
 
 /*
 |--------------------------------------------------------------------------
