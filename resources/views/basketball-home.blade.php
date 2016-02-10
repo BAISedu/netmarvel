6edb8058ee9d1316cdf87564bd1af0895f1a4dc4
Brenden Lyon <brendenlyon@internal.baisedu.org>
2016-02-10 08:40:27 +0700

    Updated basketball home page

--- resources/views/basketball-home.blade.php
+++ resources/views/basketball-home.blade.php
@@ -5,4 +5,27 @@
         <div class="title">Basketball {{ $year }}</div>
         @include('layouts.bb-nav')
     </div>
+    <h3>asl;jjdfhlajjdfhljlshdfkjhddfkljshdhffkjhsddfkjjasddkjfjssddvvbevuoweoobbvjjbbsdoiufuhwefoudsdkvjjasdkjjvbv;ajjsdhffkjadhskhbhvoubadhflkajsdhfkjjashdfkjbwfbweeoiuvjb;qwjjbbfbou'qkjbf
+    sakjdfhiuwqbkjbv
+    fkjasdfhouuwb
+    sdbvvwiugefgfousdh
+    wejkbfiuBDWVW
+    nqlwughfiuwbbgfq
+    wdljfbiwuegfq
+    qwdjfaisudgfga
+    gjabriugbawjknnv
+    ajdvbiuwghefg
+    adbiubwiruvbrw
+    avbiwbvuoqwhhgg
+    aksjjbviwuevvouw
+    ajbviuowbevuwbv
+    absvwuvuvbbv
+    skkdjbvv;iiwbvouabbv
+    swbjv;ioiawu;uba
+    w;;kvnhwbvuiyas
+    asvlkhkasbddviabv
+
+    asddkvvhhbaivbwiuvv
+    askdvbbivbiuwref
+    </h3>
 @endsection
