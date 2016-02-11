71d0b13be2abbdd719a0427c6967fe63b1620ae1
Justyn Tenandar <justyntenandar@internal.baisedu.org>
2016-02-11 09:18:20 +0700

    Did so much

--- resources/views/bob-home.blade.php
+++ resources/views/bob-home.blade.php
@@ -24,6 +24,34 @@
 <div class="row">
     <div class="col-sm-9">
         <h3>Upcoming Matches</h3>
+    
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-5 text-left">Team 1</div>
+                        <div class="col-xs-2 text-center">VS</div>
+                        <div class="col-xs-5 text-right">Team 2</div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                     <div class="row">
+                        <div class="col-xs-5 text-left">Team 3</div>
+                        <div class="col-xs-2 text-center">VS</div>
+                        <div class="col-xs-5 text-right">Team 4</div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-5 text-left">Team 1</div>
+                        <div class="col-xs-2 text-center">VS</div>
+                        <div class="col-xs-5 text-right">Team 3</div>
+                    </div>
+                </li>
+                </li>
+                <li class="list-group-item"><p>Team 2 vs Team 4</p></li>
+            </ul>
+    
+    
     </div>
     <div class="col-sm-3">
         <h3> Current Match </h3>
