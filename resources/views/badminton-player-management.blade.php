4d364761b8094e43d63ba466201a41b3188b91cd
Ji Ho Yoo <jihoyoo@internal.baisedu.org>
2016-02-11 09:18:08 +0700

    Started working on the player management

---
+++ resources/views/badminton-player-management.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title', 'Welcome to Badminton!')
+@section('content')
+<div class="content">
+    <div class="title"><h1>Badminton &raquo; Player Management</h1></div>
+    @include('layouts/badminton-nav');
+    <div class="container">
+        
+    </div>
+</div>
+@endsection
