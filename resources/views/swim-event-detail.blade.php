45e11954995e33bf0f1353ec7bd9fdcd0435d627
Su Hyeon Im <suhyeonim@internal.baisedu.org>
2016-02-11 09:18:01 +0700

    Added a new event-detail template using collapsible panel

--- resources/views/swim-event-detail.blade.php
+++ resources/views/swim-event-detail.blade.php
@@ -5,44 +5,40 @@
     <div class="title"><h1>Swim</h1></div>
     <h2>Event Details</h2>
     <div class="container-fluid">
-        <div class="row">
-            <div class="table-responsive">
-            <table class="table table-striped">
-            <thead>
-            <tr>
-            <th>PID</th>
-            <th>Programme Name</th>
-            <th>Venue</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-            <td>PID#</td>
-            <td>IISSAC Swim Meet Preliminary</td>
-            <td><a href="#">Bandung Allience Intercultural School</a></td>
-            <td>yy-mm-dd</td>
-            <td>yy-mm-dd</td>
-            </tr>
-            <tr>
-            <td>PID#</td>
-            <td>IISSAC Swim Meet 2015</td>
-            <td>Bandung Allience Intercultural School</td>
-            <td>yy-mm-dd</td>
-            <td>yy-mm-dd</td>
-            </tr>
-            <tr>
-            <td>PID#</td>
-            <td>Programme</td>
-            <td>Bandung Allience Intercultural School</td>
-            <td>yy-mm-dd</td>
-            <td>yy-mm-dd</td>
-            </tr>
-            </tbody>
-            </table>
+        <div class="panel-group" id="accordion">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Event 01</a>
+                    </h4>
+                </div>
+                <div id="collapse1" class="panel-collapse collapse in">
+                    <div class="panel-body">
+                        <!-- table content goes in here -->
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Lane</th>
+                                    <th>Heat</th>
+                                    <th>Name</th>
+                                    <th>Team</th>
+                                    <th>Sub.Time</th>
+                                    <th>Time(mm:ss:ms)</th>
+                                    <th>Place</th>
+                                </tr>
+                            </thead>
+                            <thead>
+                                <tr>
+                                    <th>Heat 1</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
-@
+<script type="text/javascript">
+</script>
+@endsection
