728f1abe5102e942288134a32aa8d8b4c4d17383
Matthew Shin <matthewshin@internal.baisedu.org>
2016-02-12 08:59:59 +0700

    added the Columm for Stats,Current Gmae, and Player Managements

--- resources/views/crosscountry-home.blade.php
+++ resources/views/crosscountry-home.blade.php
@@ -4,7 +4,30 @@
 <div class="content">
     <div class="title"><h1>Cross Country</h1></div>
     @include('layouts/cross-country-nav');    
-
+    <div class="container">
+        <div class="row">
+        <div class="col-md-4">
+            <h2>Stats</h2>
+            <p>See the Stats</p>
+            <p>
+                <a class="btn btn-default" href="#" role="button">View details </a>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>Current Game</h2>
+            <p>Score of current games going on</p>
+            <p>
+                <a class="btn btn-default" href="#" role="button">View details</a>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>Player Management</h2>
+            <p> Adding Players and Events</p>
+            <p>
+                <a class="btn btn-default" href="#" role="button">Add Data</a>
+            </p>
+        </div>
+    </div>
 
 </div>
 @endsection
