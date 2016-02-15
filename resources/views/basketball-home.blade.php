e42153a4d5a9438d59a6f3e55dc2e4c02b7890c3
Brenden Lyon <brendenlyon@internal.baisedu.org>
2016-02-15 09:26:19 +0700

    Added the code for jquery-bracket sample

--- resources/views/basketball-home.blade.php
+++ resources/views/basketball-home.blade.php
@@ -14,9 +14,7 @@
             <button type="button" class="btn btn-info">Edit Event</button>
         </div>
     </div>
-    <div><!-- braxket -->
-         
-
+    <div id="bracket"><!-- bracket -->
     </div>
     <div> <!-- upcoming games -->
         <div class="col-sm-6">
@@ -43,5 +41,24 @@
             </div>
 
         </div>
+
+    <script type="text/javascript">
+
+    var minimalData = {
+        teams : [
+            ["Team 1", "Team 2"], /* first matchup */
+            ["Team 3", "Team 4"]  /* second matchup */
+        ],
+        results : [
+            [[1,2], [3,4]],       /* first round */
+            [[4,6], [2,1]]        /* second round */
+        ]
+    }
+
+    $(function() {
+        $('#minimal .demo').bracket({
+        init: minimalData /* data to initialize the bracket with */ })
+    })
+    </script>
 
 @endsection
