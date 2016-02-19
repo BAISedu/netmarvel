d7b0741f19041d5b67713a2c84271f223899f493
So Hyeon Park <sohyeonpark@internal.baisedu.org>
2016-02-19 09:03:09 +0700

    Worked on the table.

--- resources/views/soccer-playermanagement-BAISboys.blade.php
+++ resources/views/soccer-playermanagement-BAISboys.blade.php
@@ -95,7 +95,9 @@
     </tbody>
 </table>
 <!-- CHANGE THE SOCCER/HOMEPAGE ROUTE -->
-<a href="/soccer/playermanagement/BAISboys/AddPlayers/"><button type="button" class="btn btn-primary btn-md">Add a Player</button></a>
-<a href="/soccer/homepage/"><button type="button" class="btn btn-primary btn-md">Submit</button></a>
+<div class="text-right">
+    <a href="/soccer/playermanagement/BAISboys/AddPlayers/"><button type="button" class="btn btn-primary btn-md">Add a Player</button></a>
+    <a href="/soccer/homepage/"><button type="button" class="btn btn-primary btn-md">Submit</button></a>
+</div>
 </form>
 @endsection
