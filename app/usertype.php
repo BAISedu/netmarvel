cbb71b993584d014b80e3584211ca64a2aecd7ac
Brenden Lyon <brendenlyon@internal.baisedu.org>
2016-04-04 09:31:05 +0700

    add relationships

--- app/usertype.php
+++ app/usertype.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class usertype extends Model
 {
     //
+    protected $table = 'usertype';
+
+    public function user()
+        return $this->hasMany('app/user');
 }
