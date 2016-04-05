0b3b8a71fa9db9fcecb3348562b57406f08a55ed
Su Hyeon Im <suhyeonim@internal.baisedu.org>
2016-04-05 09:14:28 +0700

    Added relationship for swim_event and swim_evnt_heat

--- app/swim_event_heat.php
+++ app/swim_event_heat.php
@@ -10,11 +10,11 @@ class swim_event_heat extends Model
     protected $table = 'swim_event_heat';
 
     public function swim_xc_events() {
-        return $this->hasMany('app/swim_xc_event');
+        return $this->belongsTo('app/swim_xc_event');
     }
 
     public function user() {
-        return $this->hasMany('app/user.php');
+        return $this->belongsTo('app/user.php');
     }
     public function record() {
         return $this->hasMany('app/record');
--- app/swim_xc_event.php
+++ app/swim_xc_event.php
@@ -10,6 +10,6 @@ class swim_xc_event extends Model
     protected $table = 'swim_xc_event';
 
     public function swim_event_heat() {
-        return $this->belongsTo('app\swim_event_heat');
+        return $this->hasMany('app\swim_event_heat');
     }
 }
