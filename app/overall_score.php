117522e701acad1a57eedec04832b0dc17e557fa
So Hyeon Park <sohyeonpark@internal.baisedu.org>
2016-04-05 09:06:04 +0700

    I worked on database.

--- app/overall_score.php
+++ app/overall_score.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class overall_score extends Model
 {
-    //
+    protected $table = 'overall_score';
+
+    public function participating_school () {
+        return $this->belongsTo('app/participating_school');
+    }
 }
--- app/participating_school.php
+++ app/participating_school.php
@@ -9,9 +9,18 @@ class participating_school extends Model
     protected $table = 'participating_school';
 
     public function tournament_sport_venue() {
-        return $this->belongTo('app/tournament_sport_venue')
+        return $this->belongsTo('app/tournament_sport_venue')
     }
     public function team() {
         return $this->hasMany('app/team');
+    }
+    public function school () {
+        return $this->belongsTo('app/school');
+    }
+    public function mvp () {
+        return $this->hasMany('app/mvp');
+    }
+    public function overall_score () {
+        return $this->hasMany('app/overall_score');
     }
 }
