289c908f051ae4c3ea4410a061fcf49cc24606bf
Brenden Lyon <brendenlyon@internal.baisedu.org>
2016-04-12 09:07:12 +0700

    Controllers

---
+++ app/Http/Controllers/Badminton_team_user_seedController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class Badminton_team_user_seedController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Bob_user_statController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class Bob_user_statController extends Controller
+{
+    //
+}
