081f570aafa9dac9bebaca3322ed428dce3365ec
Charlie Root <root@internal.baisedu.org>
2016-04-13 09:11:24 +0700

    Rename Soccer Controller

--- app/Http/Controllers/SoccerController.php
+++ app/Http/Controllers/SoccerController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class Soccer_user_statController extends Controller
+class SoccerController extends Controller
 {
     //
 }
