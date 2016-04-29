53b19260cf414155a07715523e617e15ba39f105
Charlie Root <root@internal.baisedu.org>
2016-04-29 08:55:50 +0700

    Fixed syntax error on Form Laravel Collective

--- resources/views/basketball-login.blade.php
+++ resources/views/basketball-login.blade.php
@@ -5,27 +5,22 @@
         @include('layouts.bb-nav')
     </div>
             <body>
-                {!! Form::open(array('class' => 'login form-horizontal', 'role' => 'form')) !!}
-                <form class="login form-horizontal" role="form">
+                {!! Form::open(array('class' => 'login form-horizontal', 'role' => 'form', 'url' => '/soccer/homepage/')) !!}
                     <div class="form-group">
-                        {!! Form::label('username', 'User ID:', array('class' => "control-label col-sm-2")) !!}
+                        {!! Form::label('username', 'User ID', array('class' => "control-label col-sm-2")) !!}
                         <div class="col-sm-10">
-                            {!! Form::text('username', '', array('class' => 'form-control') !!}
-                            <input type="text" class="form-control" id="username" placeholder="Enter your user ID here." />
+                            {!! Form::text('username', null, array('class' => 'form-control', 'placeholder' => 'Enter username here')) !!}
                         </div>
                     </div>
                     <div class="form-group">
-                        {!! Form::label ('password', array('class'=> 'control-label col-sm-2') !!}
-                        <label class="control-label col-sm-2" for="PASSWORD">PASSWORD:</label>
+                        {!! Form::label ('password', 'Password', array('class'=> 'control-label col-sm-2')) !!}
                         <div class="col-sm-10">
-                            {!! Form::password ('password','',  array('class'=> 'form-control') !!}
-                            <input type="password" class="form-control" id="PASSWORD" placeholder="Enter your password here." />
+                            {!! Form::password ('password', array('class'=> 'form-control')) !!}
                         </div>
                     </div>
                     <div class="form-group">
                         <div class = "col-sm-offset-2 col-sm-10">
-                            {!! Form::Submit('Submit', array('class'=> 'btn btn-primary btn-md') !!}
-                            <a href="/soccer/homepage/"><button type="button" class="btn btn-primary btn-md">Submit</button></a>
+                            {!! Form::Submit('Submit', array('class'=> 'btn btn-primary btn-md')) !!}
                         </div>
                     </div>    
                 </form>
