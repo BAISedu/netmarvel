d4ecdaa66eac12f572ab9358567d01faf644d965
Ji Ho Yoo <jihoyoo@internal.baisedu.org>
2016-05-02 09:22:14 +0700

    sdads

--- resources/views/badminton-player-management.blade.php
+++ resources/views/badminton-player-management.blade.php
@@ -1,20 +1,16 @@
 @extends('layouts.master')
-@section('title', 'Welcome to Badminton!')
+@section('title', 'Badminton | Player Management')
 @section('content')
 <div class="content">
     <div class="title"><h1>Badminton &raquo; Player Management</h1></div>
     @include('layouts/badminton-nav')
     <div class="container">
         <h1>Player Management</h1>
-        {!! Form::open(array('id' => 'player-management')) !!}
-        <form class="school-year">
-        {!! Form::select('schools', array(null => '-- select --', 'bais' => 'BAIS', 'mcs' => 'MCS', 'sis' => 'SIS', 'wis' => 'WIS', 'bis' => 'BIS', 'bali' => 'BALI'), null, array('class' => 'form-control')) !!}
-            <select id="school-year" class="form-control">
-                <option>2013</option>
-                <option>2014</option>
-                <option>2015</option>
-                <option>2016</option>
-            </select>
+        {!! Form::open(array('id' => 'player-management', 'class' => 'school-year')) !!}
+        {!! Form::select('schools', array(null => '-- select --', 'bais' => 'BAIS', 'mcs' => 'MCS', 'sis' => 'SIS', 'wis' => 'WIS', 'bis' => 'BIS', 'bali' => 'BALI'), null, array('class' => 'form-control', 'id'=>'schools')) !!}
+        {!! Form::submit('submit', array('class' => 'hidden', 'id' => 'select-school')) !!}
+        {!! Form::close() !!}
+        {!! Form::open(array('id'=>'add-participant')) !!}
         <table class="table table-bordered">
             <thead>
                 <tr>
@@ -28,7 +24,7 @@
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row"><input type="checkbox" /></th>
+                    <th scope="row">{!! Form::checkbox('user-id', 1 ) !!}</th>
                     <td>Jiho Yoo</td>
                     <td>12</td>
                     <td>2014</td>
@@ -38,7 +34,7 @@
             </tbody>
             <tbody>
                 <tr>
-                    <th scope="row"><input type="checkbox" /></th>
+                    <th scope="row">{!! Form::checkbox('user-id', 2 ) !!}</th>
                     <td>Matthew Shin</td>
                     <td>12</td>
                     <td>2014</td>
@@ -47,11 +43,19 @@
                 </tr>
             </tbody>
         </table>
+        <div>
+        {!! Form::submit('Add Participant', array('class' => 'btn btn-default btn-lg active')) !!}
+        <button type="button" class="btn btn-default btn-lg active"><a href="/badminton/create-participant/">Create Participant</a></button>
+        </div>
+    {!! Form::close() !!}
     </div>
-    <div>
-    <button type="button" class="btn btn-default btn-lg active">Add Participant</button>
-    </form>
-    <button type="button" class="btn btn-default btn-lg active"><a href="/badminton/create-participant/">Create Participant</a></button>
     </div>
-</div>
+<script type="text/javascript">
+jQuery(function ($) {
+    $('select#schools').change(function() {
+        $('form#player-management').submit();
+        console.log('test');
+    });
+});
+</script>
 @endsection
